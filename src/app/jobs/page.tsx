b286009de019597933f87fbd239ef6d526b584c1
Naseem Ahmed <160397869+naseemahmed85@users.noreply.github.com>
2024-10-02 14:28:00 +0500

    header & footer

---
+++ src/app/jobs/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/component/header"
+import Footer from "@/component/footer"
+
+export default function(){
+    return(
+        <div>
+            <h1>I am Jobs page</h1>
+        </div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <div>My name is Naseem Ahmed Abbasi</div>
+  );
+}
